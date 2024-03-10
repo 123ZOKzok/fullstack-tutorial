89f71f6d7f0f0e52e6cf375f36cbe96a6f3213c3
123ZOKzok <zippyondiba@gmail.com>
2024-03-10 09:13:17 +0300

    creating our server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,6 @@
-require('dotenv').config();
+// We will cover using dotenv in a later lesson
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
